1d40b60f23d55fb9a44431138a2b5a8af9b92d81
TagadTeju-45 <your-emailtagadteju45@gamil.com>
2026-04-19 17:35:59 +0530

    versel fixed issue

--- community-frontend/src/main.jsx
+++ community-frontend/src/main.jsx
@@ -2,17 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter }from "react-router-dom"
+import {HashRouter }from "react-router-dom"
 import {Provider} from "react-redux"
 import store from '../store/store.js'
 createRoot(document.getElementById('root')).render(
- <BrowserRouter>
+ <HashRouter>
   {/* <StrictMode> */}
   <Provider store={store}>
 
     <App />
   </Provider>
   {/* </StrictMode>, */}
- </BrowserRouter>
+ </HashRouter>
 
 )
